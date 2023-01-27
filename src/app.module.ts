2c5fb5e573c4c6e791c7c52b24f7e7fdda1f341e
Halid-Ali <halid.ali@methodpark.de>
2023-01-27 17:28:37 +0100

    Import mongoose module with connection string

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './products/product.module';
 
 @Module({
-  imports: [ProductModule],
+  imports: [
+    ProductModule,
+    MongooseModule.forRoot('mongodb+srv://ali:ha1609@atlascluster.m9nwn7o.mongodb.net/nestjs-demo?retryWrites=true&w=majority')],
   controllers: [AppController],
   providers: [AppService],
 })
