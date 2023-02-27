fed7a921b5c686a887a9c4e8f4f00120e05c430b
Halid Ali <halidziyaali@gmail.com>
2023-02-27 15:49:42 +0100

    Token expires in 2 days

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -18,7 +18,7 @@ export class AuthService {
 
     async login(user: any) {
         const payload = { username: user.name, sub: user.id };
-        return { access_token: this.jwtService.sign(payload) };
+        return { access_token: this.jwtService.sign(payload, { expiresIn: '2d' }) };
     }
 
     private async isPasswordMatch(userPassword: string, givenPassword): Promise<boolean> {
